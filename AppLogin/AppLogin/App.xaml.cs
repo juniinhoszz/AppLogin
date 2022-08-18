deaa907725120025e99dffe752ddaa0ccba15ac5
Juninho Chiodi <juninho.junirj@gmail.com>
2022-08-18 18:58:41 -0300

    Adicionar arquivos de projeto.

---
+++ AppLogin/AppLogin/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+
+using AppLogin.View;
+using AppLogin.Model;
+
+namespace AppLogin
+{
+    public partial class App : Application
+    {
+        public List<DadosUsuario> lista_usuarios = new List<DadosUsuario>
+        {
+            new DadosUsuario()
+            {
+                Email = "aluno@etec",
+                Nome = "Aluno",
+                Senha = "123"
+            },
+            new DadosUsuario()
+            {
+                Email = "prof@etec",
+                Nome = "Professor",
+                Senha = "456"
+            }
+        };
+
+        public App()
+        {
+            InitializeComponent();
+
+            if (Properties.ContainsKey("usuario_logado"))
+                MainPage = new View.AreaProtegida();
+            else
+                MainPage = new View.Login();
+
+            //MainPage = new MainPage();
+        }
+
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
